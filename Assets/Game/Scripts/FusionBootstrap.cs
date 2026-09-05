ba08cdbb39d0e91afb3f49fcaaf1401878159f8b
Andrei Raketa <raketa420311@gmail.com>
2026-09-05 18:37:55 +0300

    Move scene under Assets/Game and add Fusion prototype objects
    Restructure assets into Assets/Game/{Scenes,Scripts}, add the
    Prototype Runner and Prototype Network Start objects to SampleScene
    and stub out FusionBootstrap.

---
+++ Assets/Game/Scripts/FusionBootstrap.cs
@@ -0,0 +1,13 @@
+﻿using Fusion;
+using UnityEngine;
+
+namespace Game.Scripts
+{
+    public sealed class FusionBootstrap : MonoBehaviour
+    {
+        private void Start()
+        {
+            gameObject.AddComponent<NetworkRunner>();
+        }
+    }
+}
